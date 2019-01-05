8af2a32f5da5958fbca9fc88f9f9611723043d03
MiroBabic <babic003@gmail.com>
2019-01-05 14:30:33 +0000

    added assets to prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,16 +23,27 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  
+  config.assets.compile = true
+  #config.serve_static_assets = true
+  config.serve_static_files = true
+  
+  config.assets.precompile += %w(vendor/assets/javascripts/*)
+  config.assets.precompile += %w(vendor/assets/stylesheets/*)
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
